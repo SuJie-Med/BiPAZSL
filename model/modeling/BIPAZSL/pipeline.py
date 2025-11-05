7b9e24ee20cfc187b05766b8e7f7e46a3e1542bb
SuJie-Med <76145745+SuJie-Med@users.noreply.github.com>
2025-11-05 20:03:44 +0800

    Update pipeline.py

--- model/modeling/BIPAZSL/pipeline.py
+++ model/modeling/BIPAZSL/pipeline.py
@@ -1,7 +1,7 @@
-from .PSVMANet import build_PSVMANet
+from .BiPAZSL import build_BiPAZSL
 
 _GZSL_META_ARCHITECTURES = {
-    "Model": build_PSVMANet,
+    "Model": build_BiPAZSL,
 }
 
 def build_gzsl_pipeline(cfg):
