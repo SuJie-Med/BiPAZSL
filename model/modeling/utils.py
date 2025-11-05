a6c7e19db9d7f2135e1e10da19da38c334962f41
SuJie-Med <76145745+SuJie-Med@users.noreply.github.com>
2025-11-05 19:58:17 +0800

    Update utils.py

--- model/modeling/utils.py
+++ model/modeling/utils.py
@@ -24,12 +24,3 @@ def get_attributes_info(name):
     else:
         info = {}
     return info
-
-
-#
-# if __name__ == '__main__':
-#
-#     ag = get_attr_group('AwA')
-#     for key in ag:
-#         t = [i+1 for i in ag[key]]
-#         print(key, t)
